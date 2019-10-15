67462c842d6d1e5e57a0a3207db90d31f5c8af2e
Artem Markov <mapuk37@icloud.com>
2019-10-15 12:17:53 +0300

    init: create repo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const chars = str.split('');
+    let stack = [];
+    let first = [];
+
+    for (let i = 0; i < chars.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j][0] === bracketsConfig[j][1] && first[j] === undefined) {            
+                  first[j] = true;
+                };
+
+            if (bracketsConfig[j][0] === chars[i] && (first[j] === true || first[j] === undefined)) {
+                stack.push(j);
+                if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                  first[j] = false;
+                };                  
+                break;
+            };
+
+            if (bracketsConfig[j][1] === chars[i] && (first[j] === undefined || first[j] === false)) {
+                let openIndex = stack.pop();
+                if (j !== openIndex) {
+                    return false;
+                };
+                if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                  first[j] = true;
+                };    
+            }
+        }
+    };
+    if (stack.length !== 0) {
+        return false;
+    };
+
+    return true;    
+};
